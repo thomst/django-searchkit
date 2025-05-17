8816cd9f65108d7b9d037c192b759bbc937b74a4
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-05-17 16:07:47 +0200

    added modeladmin and modelform for a search

--- searchkit/admin.py
+++ searchkit/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SearchkitSearch
 
-# Register your models here.
+
+@admin.register(SearchkitSearch)
+class SearchkitSearchAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_date')
---
+++ searchkit/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from searchkit.models import SearchkitSearch
+
+
+class SearchkitSearchForm(forms.ModelForm):
+    class Meta:
+        model = SearchkitSearch
+        fields = ['name']
