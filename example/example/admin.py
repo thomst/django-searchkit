67dbd5312648aa1ca31f08eb4f729eeda6c4d551
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-05-17 16:11:53 +0200

    added action and filter to the example modeladmins

--- example/example/admin.py
+++ example/example/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from searchkit.actions import searchkit_action
+from searchkit.filters import SearchkitFilter
 from .models import ModelA
 from .models import ModelB
 
@@ -6,8 +8,12 @@ from .models import ModelB
 @admin.register(ModelA)
 class ModelAAdmin(admin.ModelAdmin):
     list_display = [f.name for f in ModelA._meta.fields]
+    actions = [searchkit_action]
+    list_filter = [SearchkitFilter]
 
 
 @admin.register(ModelB)
 class ModelBAdmin(admin.ModelAdmin):
     list_display = [f.name for f in ModelB._meta.fields]
+    actions = [searchkit_action]
+    list_filter = [SearchkitFilter]
