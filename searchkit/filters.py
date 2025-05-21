9df677792a475c1c984d077649804917070389ea
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-05-21 12:47:32 +0200

    fixed the SearchkitFilter

--- searchkit/filters.py
+++ searchkit/filters.py
@@ -1,4 +1,5 @@
 from django.contrib.admin import SimpleListFilter
+from django.contrib.contenttypes.models import ContentType
 from searchkit.models import SearchkitSearch
 from searchkit.searchkit import SearchkitFormSet
 
@@ -8,10 +9,18 @@ class SearchkitFilter(SimpleListFilter):
     parameter_name = 'search'
     template = 'searchkit/filters/filter.html'
 
+    def __init__(self, request, params, model, model_admin):
+        # We need the app_label and model_name for the reverse url lookup in the
+        # template.
+        self.app_label = model._meta.app_label
+        self.model_name = model._meta.model_name
+        super().__init__(request, params, model, model_admin)
+
     def lookups(self, request, model_admin):
         # Fetch the last three objects from SearchkitSearch and return them as
         # choices.
-        searches = SearchkitSearch.objects.all().order_by('-created_date')[:3]
+        ct = ContentType.objects.get_for_model(model_admin.model)
+        searches = SearchkitSearch.objects.filter(contenttype=ct).order_by('-created_date')[:3]
         return [(str(obj.id), obj.name) for obj in searches]
 
     def queryset(self, request, queryset):
