d618049614f33192cd509bd9a1593747ac6469b2
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-05-21 09:11:47 +0200

    first sketch of a view handling the search form

--- searchkit/views.py
+++ searchkit/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
+from django.http import HttpResponseNotFound
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.apps import apps
+from django.contrib.contenttypes import ContentType
 from django.views.generic import View
+from .models import SearchkitSearch
+from .forms import SearchkitSearchForm
 from .searchkit import SearchkitFormSet
 
 
@@ -16,12 +23,52 @@ class SearchkitAjaxView(View):
         return HttpResponse(formset.render())
 
 
-# class SearchkitView(View):
+class SearchkitFilterView(View):
+    """
+    Work with the SearchkitFilter.
+    """
+    def get(self, request, app_label, model_name):
+        try:
+            model = apps.get_model(app_label=app_label, model_name=model_name)
+        except LookupError:
+            return HttpResponseNotFound(f'Page not found')
 
-#     def post(self, request, app_label, model_name):
-#         model = apps.get_model(app_label=app_label, model_name=model_name)
-#         data = request.POST.copy()
-#         indices = data['index']
-#         for index in indices:
-#             data['index'] = index
-#             form = SearchkitForm(model, data)
+        search_form = SearchkitSearchForm()
+        formset = SearchkitFormSet(model)
+
+        return render(request, 'searchkit/filter/form.html', {
+            'search_form': search_form,
+            'formset': formset,
+        })
+
+    def post(self, request, app_label, model_name):
+        try:
+            model = apps.get_model(app_label=app_label, model_name=model_name)
+        except LookupError:
+            return HttpResponseNotFound(f'Page not found')
+
+        search_form = SearchkitSearchForm(request.POST)
+        formset = SearchkitFormSet(model, request.POST)
+
+        if search_form.is_valid() and formset.is_valid():
+
+            # Save search data.
+            # We save the raw data which has been validated and can be reuesed
+            # to instantiate the formset and retrieve the filter rules.
+            search = SearchkitSearch(
+                name=search_form.cleaned_data['name'],
+                contenttype=ContentType.objects.get_for_model(model),
+                data=formset.data,
+            )
+            search.save()
+
+            # Redirect back to the change list with the search applied.
+            change_list_url = reverse(f'admin:{app_label}_{model_name}_changelist')
+            return HttpResponseRedirect(f'{change_list_url}?search={search.id}')
+
+        else:
+            # If the form is not valid, render the form again with errors.
+            return render(request, 'searchkit/filter/form.html', {
+                    'search_form': search_form,
+                    'formset': formset,
+                })
