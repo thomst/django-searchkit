1e9545605c667748ad67f7563f62b073b7aaae42
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-05-21 20:23:33 +0200

    removed admin action to add a search

--- example/example/admin.py
+++ example/example/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from searchkit.actions import searchkit_action
 from searchkit.filters import SearchkitFilter
 from .models import ModelA
 from .models import ModelB
@@ -8,12 +7,10 @@ from .models import ModelB
 @admin.register(ModelA)
 class ModelAAdmin(admin.ModelAdmin):
     list_display = [f.name for f in ModelA._meta.fields]
-    actions = [searchkit_action]
     list_filter = [SearchkitFilter]
 
 
 @admin.register(ModelB)
 class ModelBAdmin(admin.ModelAdmin):
     list_display = [f.name for f in ModelB._meta.fields]
-    actions = [searchkit_action]
     list_filter = [SearchkitFilter]
