9f3ef018f936ae41ca6af04617f807dce19650c4
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-05-22 12:47:26 +0200

    cleanup the searchkit views

--- searchkit/urls.py
+++ searchkit/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from .views import SearchkitAjaxView
-from .views import SearchkitFilterView
 
 
 urlpatterns = [
-    path("searchkit/form/<slug:app_label>/<slug:model_name>/", SearchkitAjaxView.as_view(), name="searchkit_form"),
-    path("searchkit/filter/<slug:app_label>/<slug:model_name>/", SearchkitFilterView.as_view(), name="searchkit_filter"),
+    path("searchkit/form/", SearchkitAjaxView.as_view(), name="searchkit_form"),
+    path("searchkit/form/<slug:app_label>/<slug:model_name>/", SearchkitAjaxView.as_view(), name="searchkit_form_model"),
 ]
--- searchkit/views.py
+++ searchkit/views.py
@@ -16,59 +16,24 @@ from .searchkit import SearchkitFormSet
 
 
 # FIXME: Check permissions and authentication.
-
-class GetModelMixin:
+class SearchkitAjaxView(View):
     """
-    Mixin to get the model from the URL parameters.
+    Reload the formset via ajax.
     """
     def get_model(self, **kwargs):
         """
         Get the model from the URL parameters.
         """
-        kwargs = kwargs or self.kwargs
-        app_label = kwargs['app_label']
-        model_name = kwargs['model_name']
-        try:
-            return apps.get_model(app_label=app_label, model_name=model_name)
-        except LookupError:
-            raise Http404(f'Model {app_label}.{model_name} not found')
+        if all(k in kwargs for k in ('app_label', 'model_name')):
+            app_label, model_name = kwargs['app_label'], kwargs['model_name']
+            try:
+                return apps.get_model(app_label=app_label, model_name=model_name)
+            except LookupError:
+                raise Http404(f'Model {app_label}.{model_name} not found')
+        else:
+            return None
 
-    @cached_property
-    def model(self):
-        return self.get_model()
-
-
-class SearchkitAjaxView(GetModelMixin, View):
-    """
-    Reload the formset via ajax.
-    """
     def get(self, request, **kwargs):
-        formset = SearchkitFormSet(self.model, data=request.GET)
+        model = self.get_model(**kwargs)
+        formset = SearchkitFormSet(data=request.GET, model=model)
         return HttpResponse(formset.render())
-
-
-# TODO: Is it even possible to use generic views for ModelForms here?
-class SearchkitFilterView(GetModelMixin, FormView):
-    template_name = 'searchkit/filters/form.html'
-    form_class = SearchkitSearchForm
-
-    def get_form_kwargs(self):
-        now = datetime.now().strftime("%Y-%m-%d %H:%M")
-        kwargs = super().get_form_kwargs()
-        kwargs['initial'] = kwargs.get('initial', {})
-        kwargs['initial']['name'] = f'Search for {self.model._meta.verbose_name} ({now})'
-        kwargs['initial']['contenttype'] = ContentType.objects.get_for_model(self.model)
-        return kwargs
-
-    def form_valid(self, form):
-        # Save search data.
-        search = SearchkitSearch(
-            name=form.cleaned_data['name'],
-            contenttype=form.cleaned_data['contenttype'],
-            data=form.cleaned_data['data'],
-        )
-        search.save()
-
-        # Redirect back to the change list with the search applied.
-        change_list_url = reverse(f'admin:{self.model._meta.app_label}_{self.model._meta.model_name}_changelist')
-        return HttpResponseRedirect(f'{change_list_url}?search={search.id}')
