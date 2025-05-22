5ce29c282a536085c79a25de058821aaf690d1df
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-05-22 02:05:54 +0200

    use template tags for the searchkit formset template

---
+++ searchkit/templatetags/searchkit.py
@@ -0,0 +1,31 @@
+from django.template import Library
+from django.urls import reverse
+from ..searchkit import CSS_CLASSES
+
+
+register = Library()
+
+
+@register.simple_tag
+def searchkit_url(formset):
+    """
+    Return url to reload the formset.
+    """
+    app_label = formset.model._meta.app_label
+    model_name = formset.model._meta.model_name
+    url = reverse('searchkit_form', args=[app_label, model_name])
+    return url
+
+@register.simple_tag
+def on_change_class():
+    """
+    Return css class for on change handler.
+    """
+    return CSS_CLASSES['reload_on_change']
+
+@register.simple_tag
+def on_click_class():
+    """
+    Return css class for on click handler.
+    """
+    return CSS_CLASSES['reload_on_click']
