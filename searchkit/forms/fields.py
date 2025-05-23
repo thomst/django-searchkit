9eada6332392cfc9425d4ab12b9944cea37d7cda
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-05-23 13:57:09 +0200

    simplify fields code

--- searchkit/forms/fields.py
+++ searchkit/forms/fields.py
@@ -12,63 +12,44 @@ class RangeWidget(forms.MultiWidget):
 
 
 class BaseRangeField(forms.MultiValueField):
+    incomplete_message = None
+    field_type = None
+    widget_type = None
+    field_kwargs = dict()
+
+    def __init__(self, **kwargs):
+        error_messages = dict(incomplete=self.incomplete_message)
+        widget = RangeWidget(widgets=[self.widget_type, self.widget_type])
+        fields = (
+            self.field_type(label=_('From'), **self.field_kwargs),
+            self.field_type(label=_('To'), **self.field_kwargs),
+        )
+        super().__init__(
+            fields=fields,
+            widget=widget,
+            error_messages=error_messages,
+            require_all_fields=True,
+            **kwargs
+        )
+
     def compress(self, data_list):
         """We want the data list as data list."""
         return data_list
 
 
 class IntegerRangeField(BaseRangeField):
-    def __init__(self, **kwargs):
-        error_messages = {
-            "incomplete": _("Enter the first and the last number."),
-        }
-        fields = (
-            forms.IntegerField(label=_('From')),
-            forms.IntegerField(label=_('To')),
-        )
-        widget = RangeWidget(widgets=[forms.NumberInput, forms.NumberInput])
-        super().__init__(
-            fields=fields,
-            widget=widget,
-            error_messages=error_messages,
-            require_all_fields=True,
-            **kwargs
-        )
+    incomplete_message = _("Enter the first and the last number.")
+    field_type = forms.IntegerField
+    widget_type = forms.NumberInput
 
 
 class DateRangeField(BaseRangeField):
-    def __init__(self, **kwargs):
-        error_messages = {
-            "incomplete": _("Enter the first and the last date."),
-        }
-        fields = (
-            forms.DateField(label=_('From')),
-            forms.DateField(label=_('To')),
-        )
-        widget = RangeWidget(widgets=[forms.DateInput, forms.DateInput]),
-        super().__init__(
-            fields=fields,
-            widget=widget,
-            error_messages=error_messages,
-            require_all_fields=True,
-            **kwargs
-        )
+    incomplete_message = _("Enter the first and the last date.")
+    field_type = forms.DateField
+    widget_type = forms.DateInput
 
 
 class DateTimeRangeField(BaseRangeField):
-    def __init__(self, **kwargs):
-        error_messages = {
-            "incomplete": _("Enter the first and the last datetime."),
-        }
-        fields = (
-            forms.DateTimeField(label=_('From')),
-            forms.DateTimeField(label=_('To')),
-        )
-        widget = RangeWidget(widgets=[forms.DateTimeInput, forms.DateTimeInput]),
-        super().__init__(
-            fields=fields,
-            widget=widget,
-            error_messages=error_messages,
-            require_all_fields=True,
-            **kwargs
-        )
+    incomplete_message = _("Enter the first and the last datetime.")
+    field_type = forms.DateTimeField
+    widget_type = forms.DateTimeInput
