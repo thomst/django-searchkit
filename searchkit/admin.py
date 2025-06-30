b1d6d6acc8aa9f20e9dc8dd9bbf5f0fc4f2dec20
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-06-30 17:42:03 +0200

    apply view for the search model admin

--- searchkit/admin.py
+++ searchkit/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils.html import format_html
 from .models import Search
 from .forms import SearchForm
 from .filters import SearchkitFilter
@@ -9,7 +10,7 @@ from .filters import SearchkitFilter
 @admin.register(Search)
 class SearchkitSearchAdmin(admin.ModelAdmin):
     form = SearchForm
-    list_display = ('name', 'contenttype', 'created_date')
+    list_display = ('name', 'contenttype', 'created_date', 'apply_search_view')
 
     def get_url_for_applied_search(self, obj):
         app_label = obj.contenttype.app_label
@@ -28,3 +29,14 @@ class SearchkitSearchAdmin(admin.ModelAdmin):
             return HttpResponseRedirect(self.get_url_for_applied_search(obj))
         else:
             return super().response_change(request, obj, *args, **kwargs)
+
+    def apply_search_view(self, obj):
+        """
+        Returns a link to apply the search.
+        """
+        return format_html(
+            '<a href="{}" >Apply search "{}"</a>',
+            self.get_url_for_applied_search(obj),
+            obj.name
+        )
+    apply_search_view.short_description = 'Apply Search'
