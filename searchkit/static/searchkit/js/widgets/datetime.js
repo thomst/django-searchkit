3a9611be1d00bed37963597d50f63e7a4a6cf090
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-07-07 22:00:50 +0200

    reformat the js code

--- searchkit/static/searchkit/js/widgets/datetime.js
+++ searchkit/static/searchkit/js/widgets/datetime.js
@@ -1,5 +1,9 @@
 "use strict";
+
+// Initialize datetime widgets when searchkit got reloaded.
+
 {
-    // Initialize datetime widgets when searchkit got reloaded.
+
     document.addEventListener("searchkit:reloaded", (e) => DateTimeShortcuts.init());
+
 }
