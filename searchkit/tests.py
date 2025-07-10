49d25ada849d4d2913bee739d69cc8bd3e5b610e
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-07-10 11:45:59 +0200

    fixed tests

--- searchkit/tests.py
+++ searchkit/tests.py
@@ -1,4 +1,7 @@
 import os, sys
+from pprint import pprint
+from decimal import Decimal
+from contextlib import contextmanager
 from urllib.parse import urlencode
 from django.test import TestCase
 from django.contrib.contenttypes.models import ContentType
@@ -6,8 +9,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from example.models import ModelA
 from example.management.commands.createtestdata import Command as CreateTestData
-from searchkit.utils import FIELD_PLAN
-from searchkit.utils import SUPPORTED_FIELDS
+from searchkit.forms import FieldPlan
 from searchkit.utils import ModelTree
 from searchkit.forms import SearchForm
 from searchkit.forms import SearchkitModelForm
@@ -28,9 +30,39 @@ SearchkitForm = SearchkitFormSet.form
 
 INITIAL_DATA = [
     dict(
-        field='model_b__chars',
-        operator='contains',
-        value='anytext',
+        field='id',
+        operator='gt',
+        value=123,
+    ),
+    dict(
+        field='boolean',
+        operator='exact',
+        value=True,
+    ),
+    dict(
+        field='chars',
+        operator='in',
+        value=['ModelA chars 1', 'ModelA chars 2'],
+    ),
+    dict(
+        field='chars_choices',
+        operator='exact',
+        value='one',
+    ),
+    dict(
+        field='email',
+        operator='startswith',
+        value='user12',
+    ),
+    dict(
+        field='url',
+        operator='regex',
+        value='^.+\.com/[a-z]+/66$',
+    ),
+    dict(
+        field='uuid',
+        operator='endswith',
+        value='x',
     ),
     dict(
         field='integer',
@@ -38,19 +70,34 @@ INITIAL_DATA = [
         value=[1, 123],
     ),
     dict(
-        field='float',
+        field='big_integer',
         operator='gt',
-        value='0.3',
+        value=1000,
+    ),
+    dict(
+        field='integer_choices',
+        operator='exact',
+        value=3,
+    ),
+    dict(
+        field='float',
+        operator='lte',
+        value=10.02,
     ),
     dict(
         field='decimal',
-        operator='lte',
-        value='1.23',
+        operator='gte',
+        value=Decimal('2.2'),
     ),
     dict(
         field='date',
-        operator='exact',
-        value='2025-05-14',
+        operator='gt',
+        value='2020-05-14',
+    ),
+    dict(
+        field='time',
+        operator='lte',
+        value='22:45',
     ),
     dict(
         field='datetime',
@@ -66,22 +113,36 @@ DEFAULT_PREFIX = SearchkitFormSet.get_default_prefix()
 def get_form_data(initial_data=INITIAL_DATA):
     count = len(initial_data)
     data = {
-        'name': 'test search',                  # The name of the search.
-        'searchkit_model': f'{contenttype.pk}',  # Data for the searchkit-model form.
-        f'{DEFAULT_PREFIX}-TOTAL_FORMS': f'{count}',   # Data for the managment form.
-        f'{DEFAULT_PREFIX}-INITIAL_FORMS': f'{count}', # Data for the managment form.
+        'name': 'test search',                          # The name of the search.
+        'searchkit_model': f'{contenttype.pk}',         # Data for the searchkit-model form.
+        f'{DEFAULT_PREFIX}-TOTAL_FORMS': f'{count}',    # Data for the managment form.
+        f'{DEFAULT_PREFIX}-INITIAL_FORMS': f'{count}',  # Data for the managment form.
     }
-    for i, d in enumerate(initial_data):
+    for i, idata in enumerate(initial_data):
         prefix = SearchkitFormSet().add_prefix(i)
-        for key, value in d.items():
-            if isinstance(value, list):
-                for i, v in enumerate(value):
-                    data.update({f'{prefix}-{key}_{i}': v})
+        for key, value in idata.items():
+            # Create multiple value fields for range operators or the datetime
+            # field. Exclude the in operator since it expects a list as value.
+            if isinstance(value, list) and idata['operator'] != 'in':
+                data.update({f'{prefix}-{key}_{0}': value[0]})
+                data.update({f'{prefix}-{key}_{1}': value[1]})
             else:
                 data.update({f'{prefix}-{key}': value})
     return data
 
 FORM_DATA = get_form_data()
+
+
+@contextmanager
+def silence_stdout():
+    old_stdout = sys.stdout
+    try:
+        with open(os.devnull, "w") as dev_null:
+            sys.stdout = dev_null
+            yield dev_null
+    finally:
+        sys.stdout = old_stdout
+
 
 
 class CheckFormMixin:
@@ -102,23 +163,22 @@ class CheckFormMixin:
         tree = ModelTree(ModelA)
         for node in tree.iterate():
             for model_field in node.model._meta.fields:
-                if not any(isinstance(model_field, f) for f in SUPPORTED_FIELDS):
+                if not any(isinstance(model_field, f) for f in FieldPlan.SUPPORTED_FIELD_TYPES):
                     continue
                 if node.is_root:
                     self.assertIn(model_field.name, options)
                 else:
                     self.assertIn(f'{node.field_path}__{model_field.name}', options)
 
-        # Check the field_plan choosen based on the model_field.
-        field_plan = next(iter([p for t, p in FIELD_PLAN.items() if t(form.model_field)]))
-        self.assertEqual(form.field_plan, field_plan)
+        # Check operator field has choices.
+        self.assertTrue(form.fields['operator'].choices)
 
-        # Check choices of the operator field based on the field_plan.
-        operator_field = form.fields['operator']
-        self.assertTrue(operator_field.choices)
-        self.assertEqual(len(operator_field.choices), len(form.field_plan))
-        for operator in form.field_plan.keys():
-            self.assertIn(operator, [c[0] for c in operator_field.choices])
+
+class CreateTestDataMixin:
+    @classmethod
+    def setUpTestData(cls):
+        with silence_stdout():
+            CreateTestData().handle()
 
 
 class SearchkitFormTestCase(CheckFormMixin, TestCase):
@@ -210,7 +270,7 @@ class SearchkitFormTestCase(CheckFormMixin, TestCase):
         self.assertIn(errors, form.errors.values())
 
 
-class SearchkitFormSetTestCase(CheckFormMixin, TestCase):
+class SearchkitFormSetTestCase(CreateTestDataMixin, CheckFormMixin, TestCase):
     def test_blank_searchkitform(self):
         # Instantiating the formset neither with a model instance nor with model
         # related data or initial data should result in a formset without forms,
@@ -244,7 +304,7 @@ class SearchkitFormSetTestCase(CheckFormMixin, TestCase):
             self.check_form(form)
 
 
-class SearchkitSearchFormTestCase(TestCase):
+class SearchkitSearchFormTestCase(CreateTestDataMixin, TestCase):
     def test_searchkit_search_form_without_data(self):
         form = SearchForm()
         self.assertFalse(form.is_bound)
@@ -281,11 +341,7 @@ class SearchkitModelFormTestCase(TestCase):
         self.assertEqual('example | model b', labels[2].lower())
 
 
-class AdminBackendTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        with open(os.devnull, 'w') as sys.stdout:
-            CreateTestData().handle()
+class AdminBackendTest(CreateTestDataMixin, TestCase):
 
     def setUp(self):
         admin = User.objects.get(username='admin')
@@ -300,13 +356,14 @@ class AdminBackendTest(TestCase):
             self.assertIn(snippet, resp.content)
 
     def test_search_form_with_initial(self):
-        url = reverse('admin:searchkit_search_add') + '?searchkit_model=1'
+        model_id = ContentType.objects.get_for_model(ModelA).id
+        url = reverse('admin:searchkit_search_add') + f'?searchkit_model={model_id}'
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
         select = '<select name="searchkit_model" class="searchkit-reload" data-reload-handler="change" data-total-forms="1" required id="id_searchkit_model">'
         for snippet in select.split(' '):
             self.assertIn(snippet, str(resp.content))
-        self.assertIn('<option value="1" selected>', str(resp.content))
+        self.assertIn(f'<option value="{model_id}" selected>', str(resp.content))
         self.assertIn('name="searchkit-example-modela-0-field"', str(resp.content))
 
     def test_add_search(self):
@@ -336,12 +393,7 @@ class AdminBackendTest(TestCase):
         self.assertIn(data['name'], str(resp.content))
 
 
-class SearchkitViewTest(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        with open(os.devnull, 'w') as sys.stdout:
-            CreateTestData().handle()
+class SearchkitViewTest(CreateTestDataMixin, TestCase):
 
     def setUp(self):
         admin = User.objects.get(username='admin')
