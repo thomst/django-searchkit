a7e68a8ff3a257117a49ae013c7e9db2931f09ad
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-07-11 09:04:27 +0200

    improved model admins for example project

--- example/example/admin.py
+++ example/example/admin.py
@@ -2,15 +2,22 @@ from django.contrib import admin
 from searchkit.filters import SearchkitFilter
 from .models import ModelA
 from .models import ModelB
+from .models import ModelC
 
 
 @admin.register(ModelA)
 class ModelAAdmin(admin.ModelAdmin):
-    list_display = [f.name for f in ModelA._meta.fields]
+    list_display = [f.name for f in ModelA._meta.fields if not f.name == 'text']
     list_filter = [SearchkitFilter]
 
 
 @admin.register(ModelB)
 class ModelBAdmin(admin.ModelAdmin):
     list_display = [f.name for f in ModelB._meta.fields]
+    list_filter = [SearchkitFilter]
+
+
+@admin.register(ModelC)
+class ModelCAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in ModelC._meta.fields]
     list_filter = [SearchkitFilter]
