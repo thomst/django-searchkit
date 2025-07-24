bd664c2984b0cdb6d7948734be1d79905753564b
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-07-24 14:01:51 +0200

    refactored the as_fieldset template tag

--- searchkit/templatetags/searchkit.py
+++ searchkit/templatetags/searchkit.py
@@ -5,14 +5,14 @@ from django.contrib.admin.helpers import Fieldset
 register = Library()
 
 @register.inclusion_tag("admin/includes/fieldset.html")
-def as_fieldset(form, heading_level=2, prefix='', id_prefix=0, id_suffix='', **fieldset_kwargs):
+def as_fieldset(form, name, prefix='searchkit', id_prefix=0, id_suffix=0):
     """
     Create and render a fieldset for form.
     """
-    fieldset = Fieldset(form, fields=form.fields, **fieldset_kwargs)
+    fieldset = Fieldset(form, name=name, fields=form.fields)
     context = dict(
         fieldset=fieldset,
-        heading_level=heading_level,
+        heading_level=2,
         prefix=prefix,
         id_prefix=id_prefix,
         id_suffix=id_suffix,
